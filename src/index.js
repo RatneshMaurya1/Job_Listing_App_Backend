8eb0b5e6791c525fad18038de359a861a4a8ab4b
RatneshMaurya1 <mauryaratnesh022@gmail.com>
2024-11-11 15:58:27 +0530

    bug fix

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000
 const corsOption = {
     origin: [
         "http://localhost:5173",
-        "https://job-listing-app-frontend-ratnesh.vercel.app/"
+        "https://job-listing-app-frontend-ratnesh.vercel.app"
     ],
     credentials: true,
 }
