b9bf8aa30b9920334fa65fd235eabbd4959662b7
RatneshMaurya1 <mauryaratnesh022@gmail.com>
2024-11-11 15:50:07 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 const corsOption = {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://job-listing-app-frontend-ratnesh.vercel.app/"],
     credentials: true,
 }
 app.use(cors(corsOption))
