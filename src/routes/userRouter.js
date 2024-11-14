ffe8c9d3b26d1ee077c12dfc24d6f62489ad4e59
RatneshMaurya1 <mauryaratnesh022@gmail.com>
2024-11-14 21:51:10 +0530

    updated user route

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -32,7 +32,7 @@ userRouter.post("/signup",async (req,res) => {
         phone
     })
     const token = JWT.sign({email}, process.env.SECRET, )
-    res.cookie("token",token,{httpOnly:false,sameSite:"lax",secure:false})
+    res.cookie("token",token)
     await user.save()
     return res.status(200).json({message:"user created successfully",
         status:"200",
@@ -57,7 +57,7 @@ userRouter.post("/signin", async (req,res) => {
         throw new Error("invalid email or password")
     }
     const token = JWT.sign({email}, process.env.SECRET)
-    res.cookie("token",token,{httpOnly:false,sameSite:"lax",secure:false})
+    res.cookie("token",token)
     return res.json({message: "Logged in successfully",
         user,
         token
